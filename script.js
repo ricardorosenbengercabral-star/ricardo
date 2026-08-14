b28a6a8359930806abe2d2817dd8aa82107ef470
ricardorosenbengercabral-star <ricardo.rosenbenger.cabral@escola.pr.gov.br>
2026-08-13 21:47:34 -0300

    Remove button click event listener
    Remove click event listener for button navigation.

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("btn-proximo")) {
-    const atual = document.querySelector(".ativo");
-    const proximoId = "passo-" + e.target.dataset.proximo;
 
-    atual.classList.remove("ativo");
-    document.getElementById(proximoId).classList.add("ativo");
-  }
-});
